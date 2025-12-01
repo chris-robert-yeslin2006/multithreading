32997c5482df30c9a6d4eb792a1c9518e3df4b92
chris-robert-yeslin2006 <robertchemist2006@gmail.com>
2025-12-01 21:41:55 +0530

    thread extends with Thread class

---
+++ thread_implementation/ExtendsThread.java
@@ -0,0 +1,26 @@
+class ExtendsThread{
+    public static void main(String[] args){
+        Thread t1=new Thread1();
+        Thread t2=new Thread2();
+        t1.start();
+        t2.start();
+
+
+    }
+}
+class Thread1 extends Thread{
+    @Override
+    public void run(){
+        for(int i=0;i<10;i++){
+            System.out.println("from thread 1:"+i);
+        }
+    }
+}
+class Thread2 extends Thread{
+    @Override
+    public void run(){
+        for(int i=0;i<20;i++){
+            System.out.println("from thread 2:"+i);
+        }
+    }
+}
